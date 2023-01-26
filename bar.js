0db6032740408404feaa0864b385258ce5fee7a3
Gagandeep Singh <57759605+50R1Paps@users.noreply.github.com>
2023-01-26 16:15:21 +0100

    Barchart responsive and Linechart on right position

--- bar.js
+++ bar.js
@@ -1,65 +1,70 @@
-
-
 // append the svg object to the body of the page
-const svg3 = d3.select("#bar")
+const svg3 = d3
+  .select("#bar")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr(
+    "viewBox",
+    `0 0 ${width + margin.left + margin.right} ${
+      height + margin.top + margin.bottom
+    }`
+  )
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("width", "100%")
   .append("g")
-    .attr("transform",`translate(${margin.left},${margin.top})`);
+  .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // Parse the Data
-d3.csv("data/Female_line.csv").then( function(data) {
-
+d3.csv("data/Female_line.csv").then(function (data) {
   // List of subgroups = header of the csv files = soil condition here
-  const subgroups = data.columns.slice(1)
+  const subgroups = data.columns.slice(1);
 
   // List of groups = species here = value of the first column called group -> I show them on the X axis
-  const groups = data.map(d => d.CC)
+  const groups = data.map((d) => d.CC);
 
-  console.log(groups)
+  console.log(groups);
 
   // Add X axis
-  const x = d3.scaleBand()
-      .domain(groups)
-      .range([0, width])
-      .padding([0.2])
-  svg3.append("g")
+  const x = d3.scaleBand().domain(groups).range([0, width]).padding([0.2]);
+  svg3
+    .append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x).tickSize(0));
 
   // Add Y axis
-  const y = d3.scaleLinear()
-    .domain([40, 80])
-    .range([ height, 0 ]);
-  svg3.append("g")
-    .call(d3.axisLeft(y));
+  const y = d3.scaleLinear().domain([40, 80]).range([height, 0]);
+  svg3.append("g").call(d3.axisLeft(y));
 
   // Another scale for subgroup position?
-  const xSubgroup = d3.scaleBand()
+  const xSubgroup = d3
+    .scaleBand()
     .domain(subgroups)
     .range([0, x.bandwidth()])
-    .padding([0.05])
+    .padding([0.05]);
 
   // color palette = one color per subgroup
-  const color = d3.scaleOrdinal()
+  const color = d3
+    .scaleOrdinal()
     .domain(subgroups)
-    .range(['#e41a1c','#377eb8','#4daf4a'])
+    .range(["#e41a1c", "#377eb8", "#4daf4a"]);
 
   // Show the bars
-  svg3.append("g")
+  svg3
+    .append("g")
     .selectAll("g")
     // Enter in data = loop group per group
     .data(data)
     .join("g")
-      .attr("transform", d => `translate(${x(d.group)}, 0)`)
+    .attr("transform", (d) => `translate(${x(d.group)}, 0)`)
     .selectAll("rect")
-    .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+    .data(function (d) {
+      return subgroups.map(function (key) {
+        return { key: key, value: d[key] };
+      });
+    })
     .join("rect")
-      .attr("x", d => xSubgroup(d.key))
-      .attr("y", d => y(d.value))
-      .attr("width", xSubgroup.bandwidth())
-      .attr("height", d => height - y(d.value))
-      .attr("fill", d => color(d.key));
-
-})
+    .attr("x", (d) => xSubgroup(d.key))
+    .attr("y", (d) => y(d.value))
+    .attr("width", xSubgroup.bandwidth())
+    .attr("height", (d) => height - y(d.value))
+    .attr("fill", (d) => color(d.key));
+});
