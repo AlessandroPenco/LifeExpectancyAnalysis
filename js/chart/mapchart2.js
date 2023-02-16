d37053508b4c5a6a92fd58a328161e85d8eb165d
AlessandroPenco <alessandro.penco11@gmail.com>
2023-02-16 16:26:41 +0100

    mapchart2 done

--- js/chart/mapchart2.js
+++ js/chart/mapchart2.js
@@ -1,81 +1,135 @@
-// append the svg object to the body of the page
-var svgmapchart2 = d3.select("#mapchart2")
-.append("svg")
-  .attr(
-    "viewBox",
-    `0 0 ${width + margin.left + margin.right} ${
-      height + margin.top + margin.bottom
-    }`
-  )
-  .attr("preserveAspectRatio", "xMinYMin meet")
-  .attr("width", "100%")
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
 
-
-// Map and projection
-// const path = d3.geoPath();
-// const projection = d3.geoMercator()
-//   .scale(70)
-//   .center([0,20])
-//   .translate([width / 2, height / 2]);
-
-// Data and color scale
-const data = new Map();
-const colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-  .range(d3.schemeBlues[7]);
-
-// Load external data and boot
-Promise.all([
-d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) {
-    data.set(d.code, +d.pop)
-})]).then(function(loadData){
-    let topo = loadData[0]
-
-    let mouseOver = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .5)
-    d3.select(this)
-      .transition()
-      .duration(200)
-      .style("opacity", 1)
-      .style("stroke", "black")
-  }
-
-  let mouseLeave = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .8)
-    d3.select(this)
-      .transition()
-      .duration(200)
-      .style("stroke", "transparent")
-  }
-
-  // Draw the map
-  svgmapchart2.append("g")
-    .selectAll("path")
-    .data(topo.features)
-    .enter()
-    .append("path")
-      // draw each country
-      .attr("d", d3.geoPath()
-        .projection(projection)
+  
+function mapChart2(yy) {
+  d3.select("#mapchart2").selectAll('svg').remove()
+  d3.select("#mapchart2").selectAll('g').remove()
+  // Map and projection
+  const projection = d3.geoMercator()
+  .scale(70)
+  .center([0,20])
+  .translate([width / 2, height / 2]);
+  const path = d3.geoPath().projection(projection);
+  
+  // Load the world map data and life expectancy data
+  Promise.all([
+    d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"),
+    d3.csv("../../data/median-age.csv")
+  ]).then(function([world, data]) {
+    // Map the life expectancy data to the world map features
+    var mappedData = world.objects.countries.geometries.map(function(d) {
+      var countryData = data.find(function(e) {
+        return (e.Entity === d.properties.name && e.Year === yy);
+      });
+      if (countryData)
+      return {
+        country: d.properties.name,
+        year: +countryData.Year,
+        medianAge: +countryData.medianAge,
+        geometry: d.geometry
+      };
+      else return {
+        country: "unknown",
+        year: 0,
+        medianAge: 0};
+    });
+  
+    // Set up the color scale for life expectancy
+    var colorScale = d3.scaleSequential(d3.interpolateSpectral)
+      .domain(d3.extent(mappedData, function(d) { return d.medianAge}));
+      
+    var step = [];
+    for (let index = 1; index <= 8; index++) {
+      step.push((Math.round((d3.extent(mappedData, function(d) { return d.medianAge})[1]*index/8 + Number.EPSILON) * 100) / 100))
+    }
+    Legend(d3.scaleThreshold(step, d3.schemeSpectral[8]), "#mapchart2")
+    
+    // Set up the SVG elements for the map
+    var svgmapchart2 = d3.select("#mapchart2")
+    .append("svg")
+    .attr(
+      "viewBox",
+      `0 0 ${width + margin.left + margin.right} ${
+        height + margin.top + margin.bottom
+      }`
       )
-      // set the color of each country
-      .attr("fill", function (d) {
-        d.total = data.get(d.id) || 0;
-        return colorScale(d.total);
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("width", "100%")
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top+10})`);
+      
+      var par = "Year: " + yy;
+      svgmapchart2.append("text").text(par);
+  
+    
+    const tooltip = d3.select("body")
+      .append("div")
+      .attr("class", "d3-tooltip")
+      .style("position", "absolute")
+      .style("z-index", "10")
+      .style("visibility", "hidden")
+      .style("padding", "15px")
+      .style("background", "rgba(0,0,0,0.6)")
+      .style("border-radius", "5px")
+      .style("color", "#fff")
+      .text("a simple tooltip");
+    
+    // Draw the country shapes
+    svgmapchart2.selectAll("path")
+    .data(topojson.feature(world, world.objects.countries).features)
+    .enter()
+      .append("path")
+      .attr("d", path)
+      .style("fill", function(d) {
+        var countryData = mappedData.find(function(e) {
+          return (e.country === d.properties.name);
+        });
+        return countryData ? colorScale(countryData.medianAge) : "grey";
       })
       .style("stroke", "transparent")
-      .attr("class", function(d){ return "Country" } )
+      .attr("class", function(d){ 
+        return d.properties.name
+      })
       .style("opacity", .8)
-      .on("mouseover", mouseOver )
-      .on("mouseleave", mouseLeave )
-
-})
+      .on("mouseover", function (event, d) {
+        var countryData = mappedData.find(function(e) {
+          return (e.country === d.properties.name);
+        });
+        // make all regions' color duller and delete stroke
+        svgmapchart2.selectAll("path")
+            .style("stroke", "transparent")
+            .style("fill-opacity", "0.5")
+  
+        // make hovered ragion (id corresponding to hovered element) color normal
+        svgmapchart2.selectAll(`.${d.properties.name.replace(/\s+/g, '.')}`)
+            .style("fill-opacity", "1")
+            .style("stroke", "black")
+            .style("stroke-width", "2px")
+        // and show tooltip
+        tooltip.html(`${d.properties.name}<br>
+            Life Expectancy: ${countryData.medianAge}<br>
+            Year: ${countryData.year}<br>
+            `)
+            .style("visibility", "visible");
+    })
+    .on("mousemove", function () {
+        // move tooltip
+        tooltip.style("top", (event.pageY - 10) + "px")
+            .style("left", (event.pageX + 10) + "px");
+    })
+    .on("mouseout", function (event, d) {
+        // reset stroke and color
+        svgmapchart2.selectAll("path")
+            .style("stroke", "trasparent")
+            .style("stroke-width", "1")
+            .style("fill-opacity", "1")
+        tooltip.html(``).style("visibility", "hidden");
+    });
+      // Add the slider element and set up the event listener
+  });
+    
+  }
+  mapChart2("2018");
+  
+  var slider = d3.select("#year-slider");
+  
+  slider.on("input", function() {mapChart2(this.value)});
