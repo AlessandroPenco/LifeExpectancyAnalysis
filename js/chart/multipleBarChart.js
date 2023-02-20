52c0508064c6c7f0f350ea50883ec60d5e47557a
AlessandroPenco <alessandro.penco11@gmail.com>
2023-02-20 15:31:36 +0100

    Update multipleBarChart.js

--- js/chart/multipleBarChart.js
+++ js/chart/multipleBarChart.js
@@ -1,9 +1,9 @@
 function multipleBarChart(YY) {
   d3.select("#multipleBar").selectAll("g").remove();
-  d3.select("#barPar").selectAll("text").remove();
+  d3.select("#multipleBarPar").selectAll("text").remove();
 
   var par = "Year: " + YY;
-  d3.select("#barPar").append("text").text(par).style("font-size", "28px");
+  d3.select("#multipleBarPar").append("text").text(par).style("font-size", "28px");
   const myMultipleBar = d3.select("#multipleBar").append("g");
 
   d3.csv(
@@ -21,9 +21,33 @@ function multipleBarChart(YY) {
       (d) => (d != "continent") & (d != "Year")
     );
 
+     // color palette
+  const colors = {
+    'AF': '#fc7979',
+    'Africa': '#fc7979',
+
+    'AS': '#fae078',
+    'Asia': '#fae078',
+
+    'EU': '#80fc79',
+    'Europe': '#80fc79',
+
+    'NA': '#77f7ea',
+    'NNorth AmericaA': '#77f7ea',
+
+    'OC': '#6380f2',
+    'Oceania': '#6380f2',
+
+    'SA': '#955ffa',
+    'South America': '#955ffa',
+
+    'AllWorld': '#fc5be2',
+    'World': '#fc5be2'
+  };
+
     for (
       let continent_1 = 0;
-      continent_1 < arraycontinent.length;
+      continent_1 < arraycontinent.length-1;
       continent_1++
     ) {
       const filteredDataByContinent = arraycontinent.map((continent) => {
@@ -79,36 +103,55 @@ function multipleBarChart(YY) {
         .style("border-radius", "5px")
         .style("color", "#fff")
         .text("a simple tooltip");
-    */
+      */
       var svgBar = myMultipleBar
         .append("svg")
         .attr(
           "viewBox",
-          `0 0 ${width + margin.left + margin.right - 250} ${
+          `0 0 ${width + margin.left + margin.right - 270} ${
             height + margin.top + margin.bottom - 200
           }`
         )
         .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("width", "30%")
+        .attr("width", "50%")
         .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top + 20})`);
+        .attr("transform", `translate(${margin.left+10},${margin.top + 40})`);
 
       // Add X axis
-      const x = d3.scaleLinear().domain([0, 100]).range([0, width]);
+      const x = d3.scaleLinear().domain([0,100]).range([0, (width/3)-10]);
       svgBar
         .append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x))
+        .attr("transform", `translate(0, ${0})`)
+        .call(d3.axisTop(x))
         .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
+        .attr("transform", "translate(13,-15)rotate(-60)")
         .style("text-anchor", "end");
+
+      var country = [];
+      for (let index = 0; index < 6; index++) {
+        country.push((filteredDataByContinent.map((d) => d.data)[0])[index].Entity);
+      }
+      country[0] = "Others";
+      svgBar.append("text")
+      .attr("transform", "translate(" + (width/3)/3 + " ," + -29 + ")")
+      .style("text-anchor", "middle")
+      .style("font-size", "15")
+      .text(filteredDataByContinent[0].name)
+
+      svgBar.append("text")
+          .attr("transform", "translate(" + (width/3)/3 + " ," + -30 + ")")
+          .style("text-anchor", "middle")
+          .style("font-size", "15")
+          .style("fill", colors[filteredDataByContinent[0].name])
+          .text(filteredDataByContinent[0].name)
 
       // Y axis
       const y = d3
         .scaleBand()
-        .range([0, height])
-        .domain(filteredDataByContinent.map((d) => d.data.entity))
-        .padding(0.1);
+        .range([0, height/3])
+        .domain(country)
+        .padding(0.4);
+
       svgBar.append("g").call(d3.axisLeft(y));
       // build waffle
 
@@ -117,11 +160,44 @@ function multipleBarChart(YY) {
         .data(filteredDataByContinent[continent_1].data)
         .join("rect")
         .attr("x", (d) => x(0))
-        .attr("y", (d) => y(d.entity))
+        .attr("y", (d) => y(d.Entity=="" ? "Others" : d.Entity))
         .attr("width", (d) => d.life)
+        .attr("class", (d) => (d.Entity=="" ? "Others" : d.Entity))
         .attr("height", 10)
-        .attr("fill", "#69b3a2");
+        .attr("fill", (d) => colors[d.continent])
+        .on("mouseover", function (d, j) {
+            tooltipW.html(Math.round(j.life))
+            .style("visibility", "visible");
+            d3.select(this).attr("fill", "red");attr("fill", "red");
+        })
+        // move tooltip on move
+        .on("mousemove", function () {
+          tooltipW
+                .style("top", (event.pageY - 10) + "px")
+                .style("left", (event.pageX + 10) + "px");
+        })
+        // on mouseout: increase opacity and hide tooltip
+        .on("mouseout", function (d,j) {
+          tooltipW.html(``).style("visibility", "hidden");
+          d3.select(this).attr("fill", (d) => colors[d.continent]);
+        });
     }
   });
+
+   // add tooltip
+   const tooltipW = d3.select("body")
+   .append("div")
+   .attr("class", "d3-tooltip")
+   .style("position", "absolute")
+   .style("z-index", "10")
+   .style("visibility", "hidden")
+   .style("padding", "15px")
+   .style("background", "rgba(0,0,0,0.6)")
+   .style("border-radius", "5px")
+   .style("color", "#fff")
+   .text("a simple tooltip");
+
+
+
 }
 multipleBarChart("2018");
